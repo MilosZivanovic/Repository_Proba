07a01dfa1ff6c18709b0c40d4280147125eee538
MilosZivanovic <milanzi@sbb.rs>
2017-05-04 09:07:16 +0200

    Update stack_and_heap.h

--- stack_and_heap.h
+++ stack_and_heap.h
@@ -1,4 +1,5 @@
 #ifndef __stack_and_heap
+#define Ovde cu da dodam nesto
 #define __stack_and_heap
 
 /************************************************ For STACK_SIZE+HEAP_SIZE<=0x1000 - see Scater_file **************************************/
