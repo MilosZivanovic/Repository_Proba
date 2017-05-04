f21968e2213b59a8b5dc5954b60d3ac95eb89f2a
MilosZivanovic <milanzi@sbb.rs>
2017-05-04 11:08:02 +0200

    Update stack_and_heap.h

--- stack_and_heap.h
+++ stack_and_heap.h
@@ -1,16 +1,1 @@
-#ifndef __stack_and_heap
-#define Ovde cu da dodam nesto
-#define __stack_and_heap
-
-#define Pa cu onda i ovde da dodam nesto
-/************************************************ For STACK_SIZE+HEAP_SIZE<=0x1000 - see Scater_file **************************************/
-#define STACK_SIZE	0x400
-#define HEAP_SIZE	0x900
-/******************** Милош Живановић је Геније из Кеније **********************************/
-
-#define JOJ_JOJ_JOJ 0x400
-
-/******************** Милошe majstore svih majstora **********************************/
-
-/*** Dobro ajde i Jaza je genije ***/
-#endif
+#define Milos 2
